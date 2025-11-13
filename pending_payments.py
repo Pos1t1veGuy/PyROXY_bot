ced232ac91a17d31a72f082942bbb457423f855c
P0S1T1VE GUY <UriyDoroshenko@ya.ru>
2025-11-14 01:22:48 +0300

    bugfix

--- pending_payments.py
+++ pending_payments.py
@@ -3,13 +3,14 @@ import sqlite3
 import logging
 import asyncio
 import json
+from pathlib import Path
 from aiogram import Bot
 
 from .db_handler import Handler, SQLite_Handler, PostgresHandler
 
 
 CONFIG_FILE = 'config.json'
-config = json.load(open(CONFIG_FILE, 'r', encoding='utf-8'))
+config = json.load(open(Path(__file__).parent / CONFIG_FILE, 'r', encoding='utf-8'))
 bot = Bot(token=config["API_TOKEN"])
 logging.basicConfig(
     filename="payments.log",
@@ -24,37 +25,35 @@ async def process_pending_payments(db: Handler, do_after: Callable[[str, int, st
     payments = await db.list_pending_payments()
 
     if not payments:
-        print("Нет ожидающих оплат.")
         logger.info("Нет ожидающих оплат.")
         return
 
     for payment in payments:
         payment_id, username, details, user_id, requested_at = payment
 
-        print("\n=================")
-        print(f"ID: {payment_id}")
-        print(f"Username: {username}")
-        print(f"User ID: {user_id}")
-        print(f"Детали: {details}")
-        print(f"Запрошен: {requested_at}")
+        logger.info("\n=================\n"
+                    f"ID: {payment_id}\n"
+                    f"Username: {username}\n"
+                    f"User ID: {user_id}\n"
+                    f"Детали: {details}\n"
+                    f"Запрошен: {requested_at}\n")
 
         try:
             choice = float(input("Сколько денег начислено?: ").strip().lower())
             if choice <= 0:
                 await db.reject_payment(payment_id)
-                print("[-] Оплата отклонена.")
+                logger.info("Оплата отклонена.")
                 await do_after(username, user_id, details, choice > 0, choice)
                 continue
             else:
                 if await db.confirm_payment(payment_id, choice):
-                    print("[+] Оплата подтверждена.")
+                    logger.info("Оплата подтверждена.")
                     await do_after(username, user_id, details, choice > 0, choice)
                     continue
                 else:
-                    print("[!] Невозможно оплатить, вероятно пользователя нет в базе.")
+                    logger.warning("Невозможно оплатить, вероятно пользователя нет в базе.")
         except ValueError:
-            logger.warning(f"Некорректный ввод при оплате ID={payment_id}: {choice}")
-            print("[!] Некорректный ввод, пропуск.")
+            logger.error(f"Некорректный ввод при оплате ID={payment_id}: {choice}")
 
 
 async def callback(username: str, user_id: int, details: str, accepted: bool, money: int):
@@ -68,7 +67,6 @@ async def callback(username: str, user_id: int, details: str, accepted: bool, mo
     try:
         await bot.send_message(user_id, text)
         logger.info(f"Оплата от {username} ({user_id}) с деталями '{details}' {status}. Начислено {money}")
-        print(f"Оплата от {username} ({user_id}) с деталями '{details}' {status}.")
     except Exception as e:
         logger.error(f"Ошибка отправки уведомления пользователю {user_id}: {e}")
 
